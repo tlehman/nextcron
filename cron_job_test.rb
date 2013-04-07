a219d279a3c05570e092bc643af64d05cce80002
Tobi Lehman <tobi.lehman@gmail.com>
2013-04-06 19:07:51 -0700

    Added test around next_time method

--- cron_job_test.rb
+++ cron_job_test.rb
@@ -2,10 +2,23 @@ require 'test/unit'
 require './cron_job'
 
 class CronJobTest < Test::Unit::TestCase
-  def test_parse()
-    cron_job = CronJob.new('*/1  * * * 1-3,5-13')
+  def cron_tab # every minute
+    '*/1  * * * 1-3,5-13'
+  end
+
+  def test_parse
+    cron_job = CronJob.new(cron_tab)
     assert_equal 5, cron_job.parse.length
     assert_equal 2, cron_job.parse.last.length
+  end
+
+  def test_next_time
+    cron_job = CronJob.new(cron_tab)
+    sec, min, hour, day, month, year = Time.now.to_a[(0..6)]
+    one_minute_from_now = Time.new(year, month, day, 
+                                   hour, min, sec)
+
+    assert_equal one_minute_from_now, cron_job.next_time
   end
 end
 
