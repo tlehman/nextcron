02c266e90dcf6d79b5c31ea9e36803279be652d7
Tobi Lehman <tobi.lehman@gmail.com>
2013-04-06 18:51:38 -0700

    add crontab parsing (basic)

--- cron_job.rb
+++ cron_job.rb
@@ -1,11 +1,19 @@
 class CronJob
+  attr_reader :minutes, :hours, :day_of_month, 
+              :month, :day_of_week
+
   # raw entry is something like '*/2 * 3 * 1-2,4-5'
   def initialize(raw_entry)
     @raw_entry = raw_entry
   end
 
   def parse
-    @raw_entry.split(/\s+/).split(',')
+    @minutes,
+    @hours,
+    @day_of_month,
+    @month,
+    @day_of_week = @raw_entry.split(/\s+/).
+                              map { |e| e.split(',') }
   end
 
   def next_time
--- cron_job_test.rb
+++ cron_job_test.rb
@@ -5,6 +5,7 @@ class CronJobTest < Test::Unit::TestCase
   def test_parse()
     cron_job = CronJob.new('*/1  * * * 1-3,5-13')
     assert_equal 5, cron_job.parse.length
+    assert_equal 2, cron_job.parse.last.length
   end
 end
 
